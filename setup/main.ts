947dbec43c65b16a891ad9cbf325f040ddb95747
Ruslan.D <x.stelvia@gmail.com>
2025-02-07 12:34:30 +0600

    refactor: remove Rive Animation components and dependencies

--- setup/main.ts
+++ setup/main.ts
@@ -1,12 +1,10 @@
 import { defineAppSetup } from "@slidev/types";
 import StickyButton from "../custom-tools/sticky-notes/StickyButton.vue";
-import MyRiveAnimation from "../custom-tools/rive_animation/RiveAnimation.vue";
 import ImageWithText from "../custom-tools/image-text/ImageWithText.vue";
 import NavButton from "../custom-tools/navigation/NavButton.vue";
 
 export default defineAppSetup(({ app, router }) => {
     app.component('StickyButton', StickyButton)
-    app.component('MyRiveAnimation', MyRiveAnimation)
     app.component('ImageWithText', ImageWithText)
     app.component('NavButton', NavButton)
 })
